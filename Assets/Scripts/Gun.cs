3e60260c11ae30f16b6039e5e50bc2cfb0c3998b
willhamilton24 <ishi12@icloud.com>
2020-09-19 07:26:31 -0400

    Odds n Ends

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public int maxAmmo;
+    public int currentAmmo;
+    public int reloadTime;
+    public int drawTime;
+    public int recoil;
+    public string cartridge;
+    public PlayerController wielder;
+
+    void Awake() {
+        maxAmmo = 30;
+        currentAmmo = maxAmmo;
+    }
+
+    void Update() {
+        if(Input.GetKey(KeyCode.Mouse0))
+        {
+            Fire();
+        }
+    }
+
+    void Fire() 
+    {
+        if(currentAmmo > 0)
+        {
+            // Instantiate GameObject of Type "Projectile"
+            currentAmmo--;
+            // Apply Recoil
+            Debug.Log("Bullet Fired!");
+        }
+        else 
+        {
+            Debug.Log("Weapon Empty");
+        }
+    }
+
+    void Reload() {
+        // Reduce Player Ammo Pool
+        // Wait Reload Time
+        currentAmmo = maxAmmo;
+    }
+
+    void Draw() {
+        // Wait Draw Time
+        // Draw
+    }
+}
